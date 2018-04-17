43506caa33a04374bc040f0b7467830854a2fa1f
Max Wang <mwang@1080agile.com>
2018-04-17 10:01:49 +1000

    core support

--- build/src/PureCloudPlatform.Client.V2/Properties/AssemblyInfo.cs
+++ build/src/PureCloudPlatform.Client.V2/Properties/AssemblyInfo.cs
@@ -4,14 +4,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PureCloud Platform Client SDK")]
-[assembly: AssemblyDescription("A .NET library to interface with the PureCloud Public API")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Genesys")]
-[assembly: AssemblyProduct("PureCloudPlatformClient")]
-[assembly: AssemblyCopyright("Copyright © Genesys 2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+//[assembly: AssemblyTitle("PureCloud Platform Client SDK")]
+//[assembly: AssemblyDescription("A .NET library to interface with the PureCloud Public API")]
+//[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyCompany("Genesys")]
+//[assembly: AssemblyProduct("PureCloudPlatformClient")]
+//[assembly: AssemblyCopyright("Copyright © Genesys 2017")]
+//[assembly: AssemblyTrademark("")]
+//[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("31.1.0")]
-[assembly: AssemblyFileVersion("31.1.0")]
+//[assembly: AssemblyVersion("31.1.0")]
+//[assembly: AssemblyFileVersion("31.1.0")]
